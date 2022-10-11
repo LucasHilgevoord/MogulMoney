c2682a9b5c8e76ae69b774067521fd98866ccd16
l.hilgevoord <lucashilgevoord@hotmail.com>
2022-10-11 18:59:41 +0200

    UI

--- Assets/_Project/Scripts/GameManager.cs
+++ Assets/_Project/Scripts/GameManager.cs
@@ -6,9 +6,16 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private TriviaManager _triviaManager;
 
-
     private void Start()
     {
-        _triviaManager.StartGame();
+        //_triviaManager.StartGame();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _triviaManager.StartGame();
+        }
     }
 }
